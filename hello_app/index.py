a07ae52a0bf406378e4155bfd73ba9b96202f4e7
keer0204 <65659281+keer0204@users.noreply.github.com>
2020-05-20 20:51:20 +0530

    uploaded index.py

---
+++ hello_app/index.py
@@ -0,0 +1,37 @@
+from flask import Flask, Response, render_template
+import requests
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def supervise():
+    auth_token = '4jIbUl4sRrL8GNN0merp1KESByCrZ5HmSHiwOUEtGWSk5aSZb6sfjM9fVvOQFzIFeAUUTAVi8WHvyFd9g0hBLUOEZumyBXFhH0mUyGVEgrXmCP6UYtCe8ixbJDhuDyOZ'
+    header = {"x-api-key": auth_token, 'Content-Type': "application/json"}
+
+    project_data = {"workspaceId": 32276}
+    project_url = 'https://app.supervise.ly/public/api/v3/projects.list'
+    response = requests.get(project_url, json=project_data, headers=header).json()
+    x = list()
+    y = list()
+    for entity in response["entities"]:
+        id = entity["id"]
+        name = entity["name"]
+        x.append(id)
+        y.append(name)
+    for c, d in zip(x, y):
+        result=('{:>15}     {:<15}'.format(c, d))
+
+    df = pd.DataFrame(x,columns=['Id'])
+    df['Name'] = y
+    print(df)
+
+
+    return render_template("index.html",tables=[df.to_html(classes='data', header="true")])
+
+
+
+
+if __name__ == '__main__':
+    app.run("localhost", 8081)
