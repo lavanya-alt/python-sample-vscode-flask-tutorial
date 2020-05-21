65d30e0a368a8aed64aa979c1dd56b0db284c3af
keer0204 <65659281+keer0204@users.noreply.github.com>
2020-05-21 16:10:29 +0530

    Update views.py

--- hello_app/views.py
+++ hello_app/views.py
@@ -18,29 +18,9 @@ def contact1():
 def contact2():
     return render_template("contact2.html")
 
-@app.route("/contact")
+@app.route("/contact/")
 def contact():
-    auth_token = '4jIbUl4sRrL8GNN0merp1KESByCrZ5HmSHiwOUEtGWSk5aSZb6sfjM9fVvOQFzIFeAUUTAVi8WHvyFd9g0hBLUOEZumyBXFhH0mUyGVEgrXmCP6UYtCe8ixbJDhuDyOZ'
-    header = {"x-api-key": auth_token, 'Content-Type': "application/json"}
-
-    project_data = {"workspaceId": 32276}
-    project_url = 'https://app.supervise.ly/public/api/v3/projects.list'
-    response = requests.get(project_url, json=project_data, headers=header).json()
-    x = list()
-    y = list()
-    for entity in response["entities"]:
-        id = entity["id"]
-        name = entity["name"]
-        x.append(id)
-        y.append(name)
-
-    df = pd.DataFrame(x,columns=['Id'])
-    df['Name'] = y
-    #print(df)
-
-
-    #return render_template("contact.html",tables=[df.to_html(classes='data', header="true")])
-    return render_template("contact.html")
+     return render_template("contact.html")
 
 
 
