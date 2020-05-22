6bb3bfd527929191f43633c9e9ebaee0324bde12
keer0204 <65659281+keer0204@users.noreply.github.com>
2020-05-22 16:29:20 +0530

    Update views.py

--- hello_app/views.py
+++ hello_app/views.py
@@ -21,6 +21,28 @@ def contact2():
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
+
+@app.route("/contact/")
+def supervise():
+    auth_token = '4jIbUl4sRrL8GNN0merp1KESByCrZ5HmSHiwOUEtGWSk5aSZb6sfjM9fVvOQFzIFeAUUTAVi8WHvyFd9g0hBLUOEZumyBXFhH0mUyGVEgrXmCP6UYtCe8ixbJDhuDyOZ'
+    header = {"x-api-key": auth_token, 'Content-Type': "application/json"}
+
+    project_data = {"workspaceId": 32276}
+    project_url = 'https://app.supervise.ly/public/api/v3/projects.list'
+    response = requests.get(project_url, json=project_data, headers=header).json()
+    x = list()
+    y = list()
+    for entity in response["entities"]:
+        id = entity["id"]
+        name = entity["name"]
+        x.append(id)
+        y.append(name)
+
+    df = pd.DataFrame(x,columns=['Id'])
+    df['Name'] = y
+    #print(df)
+    return render_template("contact.html",tables=[df.to_html(classes='data', header="true")])
+
     
 
 @app.route("/hello/")
