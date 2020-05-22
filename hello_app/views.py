dc8bd3257c670966fc1ad1af27d096c0859e1198
keer0204 <65659281+keer0204@users.noreply.github.com>
2020-05-22 17:04:42 +0530

    Update views.py

--- hello_app/views.py
+++ hello_app/views.py
@@ -37,8 +37,7 @@ def contact():
     df = pd.DataFrame(x,columns=['Id'])
     df['Name'] = y
     #print(df)
-    return render_template("contact.html",tables=[df.to_html(classes='data', header="true")])
-    
+    return render_template("contact.html")    
    
 
 @app.route("/hello/")
